e58618ce6629ca74ea6b872b173fe40b5b5271bf
VinicinCanedo <vinicanedo@hotmail.com>
2024-07-16 20:00:12 -0300

    First commit

---
+++ src/test/java/com/ViniciusPortifolio/ForumApiRest/ForumApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ViniciusPortifolio.ForumApiRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
